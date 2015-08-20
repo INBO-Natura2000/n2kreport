be66cd408070bb59e28d75471c74c700c0d9bea1
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-20 17:21:44 +0200

    first attempt using CTE

--- R/read_composite_index.R
+++ R/read_composite_index.R
@@ -1,50 +1,79 @@
-  sql <- "
-    SELECT
-      Fingerprint,
-      SpeciesGroupID,
-      LocationGroupID,
-      AnalysisDate,
-      Description,
-      Estimate,
-      LCL,
-      UCL
-    FROM
-        Analysis
-      INNER JOIN
-        (
-          SELECT
-            AnalysisID,
-            Description,
-            Estimate,
-            LCL,
-            UCL
-          FROM
-              ParameterEstimate
-            INNER JOIN
-              (
-                SELECT
-                  Parameter.ID AS ID,
-                  Description
-                FROM
-                    Parameter
-                  INNER JOIN
-                    (
-                      SELECT
-                        ID
-                      FROM
-                        Parameter
-                      WHERE
-                        Description = 'Composite index'
-                    ) AS CompositeIndex
-                  ON
-                    parameter.ParentParameterID = CompositeIndex.ID
-              ) AS SelectedParameter
-            ON
-              ParameterEstimate.ParameterID = SelectedParameter.ID
-        ) AS SelectedEstimate
-      ON
-        Analysis.ID = SelectedEstimate.AnalysisID
-  "
+sql <- "
+WITH cteParameter
+AS (
+  SELECT
+    P1.ID AS ParameterID,
+    P1.Description AS Period
+  FROM
+    Parameter AS P0
+  INNER JOIN
+    Parameter AS P1
+  ON
+    P0.ID = P1.ParentParameterID
+  WHERE
+    P0.Description = 'Composite index'
+)
+
+SELECT
+  cteParameter.Period,
+  ParameterEstimate.Estimate,
+  ParameterEstimate.LCL,
+  ParameterEstimate.UCL,
+  ParameterEstimate.AnalysisID
+FROM
+  cteParameter
+INNER JOIN
+  ParameterEstimate
+ON
+  cteParameter.ParameterID = ParameterEstimate.ParameterID
+"
+sql <- "
+"
+sql <- "
+WITH cteSpeciesGroup
+AS
+(
+  SELECT
+    Scheme.Description AS Scheme,
+    SpeciesGroup.Description AS SpeciesGroup,
+    SpeciesGroup.ID AS SpeciesGroupID
+  FROM
+    Scheme
+  INNER JOIN
+    SpeciesGroup
+  ON
+    Scheme.ID = SpeciesGroup.SchemeID
+)
+
+SELECT
+  cteSpeciesGroup.Scheme AS Scheme,
+  cteSpeciesGroup.SpeciesGroup AS SpeciesGroup,
+  ModelType.Description AS ModelType,
+  Analysis.ID AS AnalysisID
+FROM
+  (
+    ModelType
+  INNER JOIN
+    ModelSet
+  ON
+    ModelType.ID = ModelSet.ModelTypeID
+  )
+INNER JOIN
+  (
+    Analysis
+  INNER JOIN
+    cteSpeciesGroup
+  ON
+    Analysis.SpeciesGroupID = cteSpeciesGroup.SpeciesGroupID
+  )
+ON 
+  ModelSet.ID = Analysis.ModelSetID
+WHERE
+  ModelType.Description LIKE 'composite index:%'
+"
   index <- sqlQuery(channel = result.channel, query = sql, stringsAsFactors = TRUE)
-  summary(index)
-  head(index, 20)
+  if (is.character(index)) {
+    cat(index)
+  } else {
+    print(index %>% as.tbl())
+  }
