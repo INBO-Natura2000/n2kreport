f1658fbc9c2678866135b894296334594c48ee38
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-21 16:11:59 +0200

    first draft of local storage

---
+++ R/refresh_persisent.R
@@ -0,0 +1,25 @@
+#' Refresh the persisent storage
+#'
+#' The original data is stored in an SQL Server database. This function caches
+#' the relevant data from the SQL Server localy.
+#' @param remote.db an open ODBC connection to the SQL Server database
+#' @param local.db a connection to a local database
+#' @export
+#' @importFrom assertthat assert_that
+#' @importFrom DBI dbWriteTable
+refresh_persisent <- function(remote.db, local.db){
+  assert_that(inherits(remote.db, "RODBC"))
+
+  composite.index <- read_composite_index(channel = remote.db)
+  composite.index$ModelType <- gsub(
+    "composite index: ",
+    "",
+    composite.index$ModelType
+  )
+  dbWriteTable(
+    conn = local.db,
+    name = "CompositeIndex",
+    value = composite.index,
+    overwrite = TRUE
+  )
+}
