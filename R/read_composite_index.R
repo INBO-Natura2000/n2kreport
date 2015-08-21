368cbde95aff2dbbd410e11b708c7f4b6312b0b5
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-21 14:07:17 +0200

    reformat sql

--- R/read_composite_index.R
+++ R/read_composite_index.R
@@ -6,113 +6,113 @@
 #' @importFrom dplyr %>% as.tbl
 read_composite_index <- function(result.channel){
   sql <- "
-  WITH cteParameter
-  AS (
-      SELECT
-        ID,
-        Description AS TopParameter,
-        Description AS BottomParameter,
-        ParentParameterID
-      FROM
-        Parameter
-      WHERE
-        Description = 'Composite index' AND
-        ParentParameterID IS NULL
-    UNION ALL
-      SELECT
-        Parameter.ID,
-        cteParameter.TopParameter AS TopParameter,
-        Parameter.Description AS BottomParameter,
-        Parameter.ParentParameterID
-      FROM
-        Parameter
-      INNER JOIN
-        cteParameter
-      ON
-        Parameter.ParentParameterID = cteParameter.ID
-  ),
-  cteSpeciesGroup
-  AS 
-  (
+WITH cteParameter
+AS (
     SELECT
-      Scheme.Description AS Scheme,
-      SpeciesGroup.Description AS SpeciesGroup,
-      SpeciesGroup.ID AS SpeciesGroupID
+      ID,
+      Description AS TopParameter,
+      Description AS BottomParameter,
+      ParentParameterID
     FROM
-      Scheme
-    INNER JOIN
-      SpeciesGroup
-    ON
-      Scheme.ID = SpeciesGroup.SchemeID
-  ),
-  cteBaseAnalysis
-  AS
-  (
-    SELECT
-      ModelType.Description AS ModelType,
-      Analysis.ID,
-      Analysis.Fingerprint,
-      Analysis.AnalysisDate,
-      AnalysisStatus.Description AS Status,
-      Analysis.SpeciesGroupID
-    FROM
-      (
-          ModelType
-        INNER JOIN
-          ModelSet
-        ON
-          ModelType.ID = ModelSet.ModelTypeID
-      )
-    INNER JOIN
-      (
-        Analysis
-      INNER JOIN
-        AnalysisStatus
-      On
-        Analysis.StatusID = AnalysisStatus.ID
-      )
-    ON 
-      ModelSet.ID = Analysis.ModelSetID
+      Parameter
     WHERE
-      ModelType.Description LIKE 'composite index:%'
-  ),
-  cteAnalysis
-  AS
-  (
+      Description = 'Composite index' AND
+      ParentParameterID IS NULL
+  UNION ALL
     SELECT
-      Scheme,
-      SpeciesGroup,
-      ModelType,
-      Fingerprint,
-      AnalysisDate,
-      Status,
-      ID
+      Parameter.ID,
+      cteParameter.TopParameter AS TopParameter,
+      Parameter.Description AS BottomParameter,
+      Parameter.ParentParameterID
     FROM
-      cteSpeciesGroup
+      Parameter
     INNER JOIN
-      cteBaseAnalysis
-    ON
-      cteSpeciesGroup.SpeciesGroupID = cteBaseAnalysis.SpeciesGroupID
-  )
-  
-  SELECT
-    cteAnalysis.*,
-    cteParameter.BottomParameter AS Period,
-    ParameterEstimate.Estimate,
-    ParameterEstimate.LCL,
-    ParameterEstimate.UCL
-  FROM 
-    cteAnalysis
-  LEFT JOIN
-    (
       cteParameter
-    INNER JOIN
-      ParameterEstimate
     ON
-      ParameterEstimate.ParameterID = cteParameter.ID
+      Parameter.ParentParameterID = cteParameter.ID
+),
+cteSpeciesGroup
+AS 
+(
+  SELECT
+    Scheme.Description AS Scheme,
+    SpeciesGroup.Description AS SpeciesGroup,
+    SpeciesGroup.ID AS SpeciesGroupID
+  FROM
+    Scheme
+  INNER JOIN
+    SpeciesGroup
+  ON
+    Scheme.ID = SpeciesGroup.SchemeID
+),
+cteBaseAnalysis
+AS
+(
+  SELECT
+    ModelType.Description AS ModelType,
+    Analysis.ID,
+    Analysis.Fingerprint,
+    Analysis.AnalysisDate,
+    AnalysisStatus.Description AS Status,
+    Analysis.SpeciesGroupID
+  FROM
+    (
+        ModelType
+      INNER JOIN
+        ModelSet
+      ON
+        ModelType.ID = ModelSet.ModelTypeID
     )
-    ON 
-  cteAnalysis.ID = ParameterEstimate.AnalysisID
+  INNER JOIN
+    (
+      Analysis
+    INNER JOIN
+      AnalysisStatus
+    On
+      Analysis.StatusID = AnalysisStatus.ID
+    )
+  ON 
+    ModelSet.ID = Analysis.ModelSetID
+  WHERE
+    ModelType.Description LIKE 'composite index:%'
+),
+cteAnalysis
+AS
+(
+  SELECT
+    Scheme,
+    SpeciesGroup,
+    ModelType,
+    Fingerprint,
+    AnalysisDate,
+    Status,
+    ID
+  FROM
+    cteSpeciesGroup
+  INNER JOIN
+    cteBaseAnalysis
+  ON
+    cteSpeciesGroup.SpeciesGroupID = cteBaseAnalysis.SpeciesGroupID
+)
+
+SELECT
+  cteAnalysis.*,
+  cteParameter.BottomParameter AS Period,
+  ParameterEstimate.Estimate,
+  ParameterEstimate.LCL,
+  ParameterEstimate.UCL
+FROM 
+  cteAnalysis
+LEFT JOIN
+  (
+    cteParameter
+  INNER JOIN
+    ParameterEstimate
+  ON
+    ParameterEstimate.ParameterID = cteParameter.ID
+  )
+  ON 
+cteAnalysis.ID = ParameterEstimate.AnalysisID
   "
   sqlQuery(
     channel = result.channel, 
